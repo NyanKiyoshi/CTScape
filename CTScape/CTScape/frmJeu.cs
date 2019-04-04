0d3678a86459d1d81862dff443b3149a48fe8785
Martin Monteil <martin.monteil@etu.unistra.fr>
2019-04-04 10:01:29 +0200

    ajout bouton table de chevet

--- CTScape/CTScape/frmJeu.cs
+++ CTScape/CTScape/frmJeu.cs
@@ -29,14 +29,26 @@ namespace CTScape
             btnIndice2.FlatAppearance.BorderColor = Color.FromArgb(246, 245, 188);
             btnIndice2.FlatAppearance.MouseOverBackColor = Color.FromArgb(246, 245, 188);
             btnIndice2.FlatAppearance.MouseDownBackColor = Color.FromArgb(246, 245, 188);
+
             btnIndice3.FlatStyle = FlatStyle.Flat;
             btnIndice3.FlatAppearance.BorderColor = Color.FromArgb(92, 52, 17);
             btnIndice2.FlatAppearance.MouseOverBackColor = Color.FromArgb(92, 52, 17);
             btnIndice2.FlatAppearance.MouseDownBackColor = Color.FromArgb(92, 52, 17);
+
             btnGateaux.FlatStyle = FlatStyle.Flat;
             btnGateaux.FlatAppearance.BorderColor = Color.FromArgb(245, 248, 193);
             btnGateaux.FlatAppearance.MouseOverBackColor = Color.FromArgb(245, 248, 193);
             btnGateaux.FlatAppearance.MouseDownBackColor = Color.FromArgb(245, 248, 193);
+
+            btnIndice4.FlatStyle = FlatStyle.Flat;
+            btnIndice4.FlatAppearance.BorderColor = Color.FromArgb(180, 134, 81);
+            btnIndice4.FlatAppearance.MouseOverBackColor = Color.FromArgb(180, 134, 81);
+            btnIndice4.FlatAppearance.MouseDownBackColor = Color.FromArgb(180, 134, 81);
+
+            btnTablechevet.FlatStyle = FlatStyle.Flat;
+            btnTablechevet.FlatAppearance.BorderColor = Color.FromArgb(180, 135, 82);
+            btnTablechevet.FlatAppearance.MouseOverBackColor = Color.FromArgb(180, 135, 82);
+            btnTablechevet.FlatAppearance.MouseDownBackColor = Color.FromArgb(180, 135, 82);
         }
 
         private void button12_Click(object sender, EventArgs e)
@@ -60,6 +72,18 @@ namespace CTScape
         private void btnGateaux_Click(object sender, EventArgs e)
         {
             MessageBox.Show("C'est pas l'heure de grignoter des gateaux ! Reviens plus tard");
+        }
+
+        private void btnIndice4_Click(object sender, EventArgs e)
+        {
+            frmIndice4 indice = new frmIndice4();
+            indice.ShowDialog();
+        }
+
+        private void btnTablechevet_Click(object sender, EventArgs e)
+        {
+            frmTableChevet indice = new frmTableChevet();
+            indice.ShowDialog();
         }
     }
 }
